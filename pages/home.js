44287bc6074dc61e7be6a9096d411712ba06dd8b
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2020-01-31 01:46:42 +0530

    Read all music files on phone

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 import { View, Text } from "native-base";
+import MusicFiles from "react-native-get-music-files";
+import { request, PERMISSIONS } from "react-native-permissions";
 
 class Home extends Component {
+  componentDidMount() {
+    request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE).then(() => {
+      console.log("Done ok");
+      MusicFiles.getAll({})
+        .then(tracks => {
+          console.log(tracks);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    });
+  }
   render() {
     return (
       <View>
