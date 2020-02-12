d98de8959c5f402a06ffa6f253ad4d28a4bfc6e2
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2020-02-13 01:23:56 +0530

    Add bottom drawer for current player

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,9 @@
-import { Body, List, ListItem, Spinner, Text } from "native-base";
+import { Body, List, ListItem, Spinner, Text, View } from "native-base";
 import React, { Component } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Dimensions } from "react-native";
 import MusicFiles from "react-native-get-music-files";
 import { PERMISSIONS, request } from "react-native-permissions";
+import BottomDrawer from "rn-bottom-drawer";
 import { Actions } from "react-native-router-flux";
 
 class Home extends Component {
@@ -22,24 +23,39 @@ class Home extends Component {
 
   render() {
     return (
-      <ScrollView>
-        {this.state.loading && <Spinner color="black" />}
-        <List>
-          {this.state.tracks.map((item, index) => (
-            <ListItem
-              key={index}
-              onPress={() => {
-                Actions.player({ filepath: item.path });
-              }}
-            >
-              <Body>
-                <Text>{item.title || item.fileName}</Text>
-                <Text note>{item.album}</Text>
-              </Body>
-            </ListItem>
-          ))}
-        </List>
-      </ScrollView>
+      <View>
+        <ScrollView>
+          {this.state.loading && <Spinner color="black" />}
+          <List>
+            {this.state.tracks.map((item, index) => (
+              <ListItem
+                key={index}
+                onPress={() => {
+                  Actions.player({ filepath: item.path });
+                }}
+              >
+                <Body>
+                  <Text>{item.title || item.fileName}</Text>
+                  <Text note>{item.album}</Text>
+                </Body>
+              </ListItem>
+            ))}
+          </List>
+        </ScrollView>
+        <BottomDrawer
+          roundedEdges={false}
+          startUp={false}
+          downDisplay={Dimensions.get("window").height - 100}
+          backgroundColor="#3e3e3e"
+          containerHeight={Dimensions.get("window").height}
+        >
+          <View>
+            <Text style={{ color: "white" }}>
+              Get directions to your location
+            </Text>
+          </View>
+        </BottomDrawer>
+      </View>
     );
   }
 }
